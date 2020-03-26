e9a6ead0379c2afee0a7bef358d23cae1047fa1c
0030481923041 <61607437+0030481923041@users.noreply.github.com>
2020-03-26 17:57:05 -0300

    Add files via upload

---
+++ Atividade3/Atividade3/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int ladoA, ladoB, ladoC;
+
+            if ((int.TryParse(txtLadoA.Text, out ladoA)) &&
+                (int.TryParse(txtLadoB.Text, out ladoB)) &&
+                (int.TryParse(txtLadoC.Text, out ladoC)))
+            {
+                if (ladoA > 0 && ladoB > 0 && ladoC > 0)
+                {
+                    if (ladoA + ladoB >= ladoC && ladoA + ladoC >= ladoB && ladoB + ladoC >= ladoA)
+                    {
+                        if (ladoA == ladoB && ladoB == ladoC && ladoC == ladoA)
+                        {
+                            MessageBox.Show("Triangulo Equilatero");
+                        }
+                        else if (ladoA == ladoB || ladoB == ladoC || ladoC == ladoA)
+                        {
+                            MessageBox.Show("Triangulo Isósceles");
+                        }
+                        else if (ladoA != ladoB && ladoB != ladoC && ladoC != ladoA)
+                        {
+                            MessageBox.Show("Triangulo Escaleno");
+                        }
+                    }
+                }
+                else
+                    MessageBox.Show("Dados inválidos!");
+            }
+            else
+                MessageBox.Show("Dados inválidos!");
+        }
+
+        private void txtLadoA_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void txtLadoB_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void txtLadoC_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+    }
+}
