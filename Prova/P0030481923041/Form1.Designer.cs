3462ce8097df5767dacbf6d54d29eeb2fb5333ce
0030481923041 <61607437+0030481923041@users.noreply.github.com>
2020-06-19 21:47:38 -0300

    Add files via upload

---
+++ Prova/P0030481923041/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace P0030481923041
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(29, 152);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(126, 88);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Verificar";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(194, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(250, 368);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 405);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Prova/P0030481923041/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace P0030481923041
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double[,] vetor = new double[1,4];
+            double totalMensal = 0;
+            double totalGeral = 0;
+            string valor = "";
+
+            for (var y = 0; y < 1; y++)
+            {
+                totalMensal = 0;
+                for (var x = 0; x < 4; x++)
+                {
+                    valor = Interaction.InputBox("Digite o total de vendas da semana " + (x + 1) + "\n" + "Referente ao mês" + (y + 1), "Entrada de dados das vendas");
+                    if (double.TryParse(valor, out vetor[y,x]))
+                    {
+                        totalMensal = vetor[y,x] + totalMensal;
+                    }
+                    else
+                    {
+                        break;
+                        x--;
+                    }
+                    listBox1.Items.Add("Mês: " + (y + 1) + " Semana: " + (x + 1) + " R$ " + (vetor[y,x].ToString("N2")) + "\n");
+                }
+                listBox1.Items.Add(">> Total do mes: " + (y + 1) + " R$ " + totalMensal.ToString("N2"));
+                listBox1.Items.Add("...................");
+                totalGeral = totalMensal + totalGeral;
+            }
+            listBox1.Items.Add(">> Total geral: " + "R$ " + totalGeral.ToString("N2"));
+        }
+    }
+}
